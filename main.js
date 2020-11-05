cb1934307cd286fd581bb3b3dab08686eff8cde1
AGBENYA Koffi Moïse <kevagb03@gmail.com>
2020-11-05 16:16:19 +0100

    test conditional rendering

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
-            url: 'https://www.intersport.fr/lot_de_6_paires_de_chaussettes_homme_everyday_cushion_crew-nike-p-SX7666~397/'
+            inventory: 8
         }
     }
 })
