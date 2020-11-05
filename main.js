8d2d4e10ef8ead0bd0be2e36255d154d2f8828d1
AGBENYA Koffi Moïse <kevagb03@gmail.com>
2020-11-05 15:55:06 +0100

    create vue app and update page dom

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: 'Adidas socks made with pure cotton and by our kind employees.'
+        }
+    }
+})
