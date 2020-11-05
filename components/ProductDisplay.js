3493c0b2e672a050a0d01d1e151deb5750ac11ae
AGBENYA Koffi Moïse <kevagb03@gmail.com>
2020-11-05 18:44:03 +0100

    test form

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -10,34 +10,36 @@ app.component('product-display', {
     /*html*/
     `<div class="product-display">
         <div class="product-container">
-        <div class="product-image">
-            <img v-bind:src="image">
-        </div>
-        <div class="product-info">
-            <h1>{{ title }}</h1>
-            <p v-if="inStock">In Stock</p>
-            <p v-else>Out of Stock</p>
+            <div class="product-image">
+                <img v-bind:src="image">
+            </div>
+            <div class="product-info">
+                <h1>{{ title }}</h1>
+                <p v-if="inStock">In Stock</p>
+                <p v-else>Out of Stock</p>
 
-            <p> Shipping: {{ shipping }} </p>
-            <ul>
-            <li v-for="detail in details">{{ detail }}</li>
-            </ul>
+                <p> Shipping: {{ shipping }} </p>
+                <ul>
+                <li v-for="detail in details">{{ detail }}</li>
+                </ul>
 
-            <div 
-            v-for="(variant, index) in variants" 
-            :key="variant.id" 
-            @mouseover="updateVariant(index)" 
-            class="color-circle"
-            :style="{backgroundColor: variant.color}"
-            ></div>
-            <button
-            class="button"
-            :class="{ disabledButton: !inStock }"
-            :disabled="!inStock"
-            @click="addToCart"
-            >Add to Cart</button>
+                <div 
+                v-for="(variant, index) in variants" 
+                :key="variant.id" 
+                @mouseover="updateVariant(index)" 
+                class="color-circle"
+                :style="{backgroundColor: variant.color}"
+                ></div>
+                <button
+                class="button"
+                :class="{ disabledButton: !inStock }"
+                :disabled="!inStock"
+                @click="addToCart"
+                >Add to Cart</button>
+            </div>
         </div>
-        </div>
+        <review-list v-if="reviews.length" :reviews="reviews"></review-list>
+        <review-form @review-submitted="addReview"></review-form>
     </div>`,
 
     data() {
@@ -49,7 +51,8 @@ app.component('product-display', {
             variants: [
                 { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
                 { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-            ]
+            ],
+            reviews: []
         }
     },
 
@@ -59,6 +62,9 @@ app.component('product-display', {
         },
         updateVariant(index) {
             this.selectedVariant = index
+        },
+        addReview(review) {
+            this.reviews.push(review)
         }
     },
 
